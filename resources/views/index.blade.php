cc843907b35ff8c21d5d9af9d606616ce4e46e95
Holycult-PC\Holycult <darko.kovacevic@croonus.com>
2020-11-12 15:44:43 +0100

    Added home page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+
+    @include('partials.slider')
+    @include('partials.aboutUs')
+    @include('partials.faq')
+    @include('partials.contacts')
+
+@endsection
---
+++ resources/views/partials/contacts.blade.php
@@ -0,0 +1,134 @@
+<section id="contacts" class="contacts-holder">
+  <div class="container">
+
+    <div class="section-title">
+      <h2>Kontakti</h2>
+      <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+    </div>
+
+    <div class="row">
+
+      <div class="col-lg-6">
+        <div class="member d-flex align-items-start">
+          <div class="row">
+            <div class="col-lg-2">
+              <div class="pic">
+                <img src="assets/img/contacts/img.jpg" class="img-fluid" alt="">   
+              </div>
+            </div>
+            <div class="col-lg-6">
+              <h4>Električar</h4>
+            <span>Petar Petrovic</span>
+            </div>
+            <div class="col-lg-4">
+              <p>064/123-12-13</p>
+              <p>064/123-12-13</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-6 mt-4 mt-lg-0">
+        <div class="member d-flex align-items-start">
+          <div class="row">
+            <div class="col-lg-2">
+              <div class="pic">
+                <img src="assets/img/contacts/img.jpg" class="img-fluid" alt="">   
+              </div>
+            </div>
+            <div class="col-lg-6">
+              <h4>Vodoinstalater</h4>
+            <span>Petar Petrovic</span>
+            </div>
+            <div class="col-lg-4">
+              <p>064/123-12-13</p>
+              <p>064/123-12-13</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-6 mt-4">
+        <div class="member d-flex align-items-start">
+          <div class="row">
+            <div class="col-lg-2">
+              <div class="pic">
+                <img src="assets/img/contacts/img.jpg" class="img-fluid" alt="">   
+              </div>
+            </div>
+            <div class="col-lg-6">
+              <h4>Stolar</h4>
+            <span>Petar Petrovic</span>
+            </div>
+            <div class="col-lg-4">
+              <p>064/123-12-13</p>
+              <p>064/123-12-13</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-6 mt-4">
+        <div class="member d-flex align-items-start">
+          <div class="row">
+            <div class="col-lg-2">
+              <div class="pic">
+                <img src="assets/img/contacts/img.jpg" class="img-fluid" alt="">   
+              </div>
+            </div>
+            <div class="col-lg-6">
+              <h4>Staklorezac</h4>
+            <span>Petar Petrovic</span>
+            </div>
+            <div class="col-lg-4">
+              <p>064/123-12-13</p>
+              <p>064/123-12-13</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-6 mt-4">
+        <div class="member d-flex align-items-start">
+          <div class="row">
+            <div class="col-lg-2">
+              <div class="pic">
+                <img src="assets/img/contacts/img.jpg" class="img-fluid" alt="">   
+              </div>
+            </div>
+            <div class="col-lg-6">
+              <h4>Prijava kvarova</h4>
+            <span>Petar Petrovic</span>
+            </div>
+            <div class="col-lg-4">
+              <p>064/123-12-13</p>
+              <p>064/123-12-13</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-6 mt-4">
+        <div class="member d-flex align-items-start">
+          <div class="row">
+            <div class="col-lg-2">
+              <div class="pic">
+                <img src="/storage/img/contacts/img.jpg" class="img-fluid" alt="">   
+              </div>   
+            </div>
+            <div class="col-lg-6">
+              <h4>Komunalna policija</h4>
+            <span>Petar Petrovic</span>
+            </div>
+            <div class="col-lg-4">
+              <p>064/123-12-13</p>
+              <p>064/123-12-13</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+</section>
