cba3deed68c02a66887f5f4ca1313aac8815e6af
Nikola Zivkovic <nikola.zivkovic@quantox.com>
2020-11-24 16:29:30 +0100

    Added location_meta fl to apartment_meta table.

--- app/ApartmentMeta.php
+++ app/ApartmentMeta.php
@@ -9,6 +9,17 @@ class ApartmentMeta extends Model
     public $table = 'apartment_meta';
 
     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    public $fillable = [
+        'apartment_id',
+        'location_meta_id',
+        'field_value'
+    ];
+
+    /**
      * Get the apartment that owns the meta data.
      */
     public function apartment()
